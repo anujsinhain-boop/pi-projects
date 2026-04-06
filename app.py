e329d5fb3d5430a3d0e7a043bf952ffd55086287
Anuj <anuj.sinha.in@gmail.com>
2026-04-06 00:18:56 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello from Raspberry Pi")
+import subprocess
+
+def scan_network():
+    result = subprocess.run(
+        ["nmap", "-sn", "-PR", "192.168.4.0/24"],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+    print(result.stdout)
+
+if __name__ == "__main__":
+    scan_network()
